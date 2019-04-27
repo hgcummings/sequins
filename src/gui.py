cf0c5e2657fb8501aa5512125013dd5acca69b8e
hgcummings <harry@xlr3.com>
2019-04-27 22:27:56 +0100

    Show more frames in UI

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,14 @@
 from guizero import App, Box, Waffle
+from os import path
+import sys
 
 
 class Gui:
     def __init__(self):
         self.app = App(title="Sequins", width=1024, height=640, bg=(0, 0, 0))
+        self.app.text_color = (216, 216, 216)
+        self.app.tk.iconbitmap(path.join(path.dirname(path.realpath(sys.argv[0])), 'logo.ico'))
+
         grid = Box(self.app, layout="grid", border=8)
         self.frames = [
             Waffle(
@@ -12,10 +17,10 @@ class Gui:
                 width=4,
                 dim=12,
                 pad=2,
-                color=(64, 32, 32),
+                color=(32, 16, 16),
                 grid=[i % 16, i // 16]
             )
-            for i in range(32)
+            for i in range(64)
         ]
 
     def start(self):
@@ -26,7 +31,7 @@ class Gui:
             frame.set_all((64, 32, 32))
 
     def display_frame(self, index, frame):
-        if index >= 32:
+        if index >= 64:
             return
 
         for note in frame.notes:
