04dd01e8dbf137ebf97245975045faa123efe82d
hgcummings <harry@xlr3.com>
2019-04-27 14:27:58 +0100

    Basic MIDI input and display

---
+++ pattern.py
@@ -0,0 +1,15 @@
+class Pattern:
+    def __init__(self):
+        self.frames = []
+        self.next_frame()
+    
+    def next_frame(self):
+        self.current_frame = Frame()
+        self.frames.append(self.current_frame)
+
+    def add_note(self, note):
+        self.current_frame.notes.append(note)
+
+class Frame:
+    def __init__(self):
+        self.notes = []
