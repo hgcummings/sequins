6d676de3afae2f3a56524dff7c179f01db1d34c6
hgcummings <harry@xlr3.com>
2019-05-03 19:19:08 +0100

    I2C driver: multiple pages

--- src/display/i2c_driver.py
+++ src/display/i2c_driver.py
@@ -2,6 +2,7 @@ from pyftdi.i2c import I2cController
 
 # Register addresses (see IS31FL3731 docs)
 _COMMAND_REGISTER = 0xfd
+_DISPLAY_REGISTER = 0x01
 _SHUTDOWN_REGISTER = 0x0a
 _FUNCTION_REGISTER = 0x0b
 _PWM_REG_OFFSET = 0x24
@@ -10,17 +11,18 @@ _PWM_REG_OFFSET = 0x24
 _BASE_ADDRESS = 0x74
 _BITS_PER_REGISTER = 8
 _DEVICE_COUNT = 4
-_COL_COUNT = 16
-_ROW_COUNT = 9
-_REGISTERS_PER_ROW = _COL_COUNT // _BITS_PER_REGISTER
+_PIXELS_PER_ROW = 16
+_PIXELS_PER_COL = 9
+_REGISTERS_PER_ROW = _PIXELS_PER_ROW // _BITS_PER_REGISTER
+_PAGE_COUNT = 8
 
 # Frame layout
 _FRAME_WIDTH = 4
 _FRAME_HEIGHT = 4
 _FRAME_ROW_SPACING = 1
-_FRAMES_PER_ROW = _COL_COUNT * _DEVICE_COUNT // _FRAME_WIDTH
-_FRAMES_PER_COL = 1 + ((_ROW_COUNT - _FRAME_HEIGHT) // (_FRAME_HEIGHT + _FRAME_ROW_SPACING))
-_TOTAL_FRAMES = _FRAMES_PER_ROW * _FRAMES_PER_COL
+_FRAMES_PER_ROW = _PIXELS_PER_ROW * _DEVICE_COUNT // _FRAME_WIDTH
+_FRAMES_PER_COL = 1 + ((_PIXELS_PER_COL - _FRAME_HEIGHT) // (_FRAME_HEIGHT + _FRAME_ROW_SPACING))
+_FRAMES_PER_PAGE = _FRAMES_PER_ROW * _FRAMES_PER_COL
 _FRAMES_PER_REGISTER = _BITS_PER_REGISTER // _FRAME_WIDTH
 
 _VELOCITY_GAMMA = [round(191 * ((((v // 4) + 1) / 32) ** 1.7)) for v in range(128)]
@@ -31,55 +33,69 @@ class I2cDriver:
         self.__monkey_patch_pyusb_backend()
         self._ctrl = I2cController()
         self._ctrl.configure('ftdi://ftdi:232h/1', frequency=400000.0)
-        self._led_registers = [[0 for _ in range(_ROW_COUNT * _REGISTERS_PER_ROW)] for _ in range(_DEVICE_COUNT)]
-        self._active_brightness_registers = [[] for _ in range(_DEVICE_COUNT)]
+        self._led_registers = self._blank_led_registers()
+        self._active_brightness_registers = [[[] for _ in range(_PAGE_COUNT)] for _ in range(_DEVICE_COUNT)]
         self._prev_frame = None
-        for matrix in range(_DEVICE_COUNT):
-            address = self._get_address_for_matrix(matrix)
+        self._current_page = None
+        for device in range(_DEVICE_COUNT):
+            address = self._get_address_for_device(device)
             port = self._ctrl.get_port(address)
             if not port.poll(relax=False):
                 raise Exception(f'Could not connect to display at address {address:#04x}')
 
             print(f'Initialising display at address {address:#04x}')
-            port.write([_COMMAND_REGISTER, 0], relax=False)
 
-            # Set all pixels to off
-            for pixel in range(_ROW_COUNT * _REGISTERS_PER_ROW):
-                port.write([pixel, 0], relax=False)
+            for page in range(_PAGE_COUNT):
+                port.write([_COMMAND_REGISTER, page], relax=False)
 
-            # Set brightness to max (ready for when we switch them on)
-            for pixel in range(_PWM_REG_OFFSET, _PWM_REG_OFFSET + _ROW_COUNT * _COL_COUNT):
-                port.write([pixel, 255], relax=False)
+                # Set all pixels to off
+                for pixel in range(_PIXELS_PER_COL * _REGISTERS_PER_ROW):
+                    port.write([pixel, 0], relax=False)
+
+                # Set brightness to max (ready for when we switch them on)
+                for pixel in range(_PWM_REG_OFFSET, _PWM_REG_OFFSET + _PIXELS_PER_COL * _PIXELS_PER_ROW):
+                    port.write([pixel, 255], relax=False)
 
             port.write([_COMMAND_REGISTER, _FUNCTION_REGISTER], relax=False)
             port.write([_SHUTDOWN_REGISTER, 1], relax=False)
 
-            # Write to frame buffer zero from here on
-            port.write([_COMMAND_REGISTER, 0], relax=True)
+        self.display_page(0)
 
     def clear(self):
-        for matrix in range(_DEVICE_COUNT):
-            port = self._ctrl.get_port(self._get_address_for_matrix(matrix))
-            for reg_addr in range(_ROW_COUNT * _REGISTERS_PER_ROW):
-                port.write([reg_addr, 0], relax=False)
-                self._led_registers[matrix][reg_addr] = 0
-            for register in self._active_brightness_registers[matrix]:
-                port.write([register, 255], relax=False)
-            self._active_brightness_registers[matrix].clear()
+        for device in range(_DEVICE_COUNT):
+            port = self._ctrl.get_port(self._get_address_for_device(device))
+            for page in range(_PAGE_COUNT):
+                port.write([_COMMAND_REGISTER, page], relax=False)
+                for reg_addr in range(_PIXELS_PER_COL * _REGISTERS_PER_ROW):
+                    port.write([reg_addr, 0], relax=False)
+                for register in self._active_brightness_registers[device][page]:
+                    port.write([register, 255], relax=False)
+                self._active_brightness_registers[device][page].clear()
+        self.display_page(0)
 
     def display_frame(self, index, frame):
-        if index >= _TOTAL_FRAMES:
+        if index >= _FRAMES_PER_PAGE * _PAGE_COUNT:
             return
 
-        (matrix, port) = self._get_matrix_for_index(index)
+        page = index // _FRAMES_PER_PAGE
+        row = ((index // _FRAMES_PER_ROW) % _FRAMES_PER_COL)
+        col = (index % _FRAMES_PER_ROW)
 
-        reg_offset = ((index % (_REGISTERS_PER_ROW * _FRAMES_PER_REGISTER)) // _FRAMES_PER_REGISTER) + (
-            (_FRAMES_PER_REGISTER * (_FRAME_HEIGHT + _FRAME_ROW_SPACING) * (index // _FRAMES_PER_ROW)))
-        shift = _FRAME_WIDTH * (index % _FRAMES_PER_REGISTER)
+        self._display_frame_at(frame, page, row, col)
 
-        for row in range(_DEVICE_COUNT):
+        self._prev_frame = (frame, page, row, col)
+
+    def _display_frame_at(self, frame, page, row, col):
+        (device, port) = self._get_device_for_column(col)
+        self.display_page(page)
+
+        reg_offset = ((col % (_REGISTERS_PER_ROW * _FRAMES_PER_REGISTER)) // _FRAMES_PER_REGISTER) + (
+            (_FRAMES_PER_REGISTER * (_FRAME_HEIGHT + _FRAME_ROW_SPACING) * row))
+        shift = _FRAME_WIDTH * (col % _FRAMES_PER_REGISTER)
+
+        for row in range(_FRAME_HEIGHT):
             reg_addr = reg_offset + _REGISTERS_PER_ROW * row
-            current = self._led_registers[matrix][reg_addr]
+            current = self._led_registers[row]
             value = current
             for col in range(_FRAME_WIDTH):
                 bit = (1 << col) << shift
@@ -89,35 +105,53 @@ class I2cDriver:
                     value = value & ~bit
             if value != current:
                 port.write([reg_addr, value], relax=True)
-                self._led_registers[matrix][reg_addr] = value
+                self._led_registers[row] = value
 
-        self._prev_frame = (index, frame)
+    def display_page(self, page):
+        if page != self._current_page:
+            print(f'Going to page {page}')
+            self._current_page = page
+            self._led_registers = self._blank_led_registers()
+            for device in range(_DEVICE_COUNT):
+                port = self._ctrl.get_port(self._get_address_for_device(device))
+                port.write([_COMMAND_REGISTER, _FUNCTION_REGISTER], relax=False)
+                port.write([_DISPLAY_REGISTER, self._current_page], relax=False)
+                port.write([_COMMAND_REGISTER, self._current_page], relax=True)
 
     def next_frame(self):
         if self._prev_frame:
-            (index, frame) = self._prev_frame
-            (matrix, port) = self._get_matrix_for_index(index)
+            (frame, page, row, col) = self._prev_frame
 
-            x_offset = _FRAME_WIDTH * (index % (_COL_COUNT // _FRAME_WIDTH))
-            y_offset = (index // _FRAMES_PER_ROW) * (_FRAME_HEIGHT + _FRAME_ROW_SPACING)
+            if (col + 1) % _FRAMES_PER_REGISTER == 0:
+                self._led_registers = self._blank_led_registers()
+
+            (device, port) = self._get_device_for_column(col)
+
+            x_offset = _FRAME_WIDTH * (col % (_PIXELS_PER_ROW // _FRAME_WIDTH))
+            y_offset = row * (_FRAME_HEIGHT + _FRAME_ROW_SPACING)
             for (pad, velocity) in enumerate(frame.velocities):
                 if velocity > 0:
                     register = (_PWM_REG_OFFSET +
-                                _COL_COUNT * (y_offset + pad // _FRAME_HEIGHT) +
+                                _PIXELS_PER_ROW * (y_offset + pad // _FRAME_HEIGHT) +
                                 (x_offset + pad % _FRAME_WIDTH))
                     port.write([register, _VELOCITY_GAMMA[velocity]], False)
-                    self._active_brightness_registers[matrix].append(register)
+                    self._active_brightness_registers[device][self._current_page].append(register)
 
             self._prev_frame = None
+        return
 
-    def _get_matrix_for_index(self, index):
-        matrix = ((index % _FRAMES_PER_ROW) // _DEVICE_COUNT)
-        port = self._ctrl.get_port(self._get_address_for_matrix(matrix))
-        return matrix, port
+    def _get_device_for_column(self, col):
+        device = col // _DEVICE_COUNT
+        port = self._ctrl.get_port(self._get_address_for_device(device))
+        return device, port
 
     @staticmethod
-    def _get_address_for_matrix(matrix):
-        return _BASE_ADDRESS + matrix
+    def _blank_led_registers():
+        return [0 for _ in range(_FRAME_HEIGHT)]
+
+    @staticmethod
+    def _get_address_for_device(device):
+        return _BASE_ADDRESS + device
 
     @staticmethod
     def __monkey_patch_pyusb_backend():
